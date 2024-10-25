1ed4cb06341f8c57e800293c94517a3c688b8c1d
David Bayode <david4bay@gmail.com>
2024-10-25 23:16:05 +0100

    nav bar

---
+++ Currency-Rate-PWA/public/src/js/app.js
@@ -0,0 +1,17 @@
+console.log("in app.js")
+
+var deferredPrompt
+
+if ('serviceWorker' in navigator) {
+    console.log("yup")
+    navigator.serviceWorker.register("../serviceWorker.js").then(function() {
+        console.log('Service Worker registered!')
+    })
+}
+
+window.addEventListener("beforeinstallprompt", function(event) {
+    console.log("beforeinstall prompt fired!")
+    event.preventDefault()
+    deferredPrompt = event 
+    return false
+})
---
+++ Currency-Rate-PWA/public/src/serviceWorker.js
@@ -0,0 +1,15 @@
+console.log("in service worker")
+
+self.addEventListener("install", function(event) {
+    console.log("[Service Worker Installing Service Worker ...", event)
+})
+
+self.addEventListener("activate", function(event) {
+    console.log("[Service Worker Activating Service Worker ...", event)
+    return self.clients.claim()
+})
+
+self.addEventListener("fetch", function(event) {
+    console.log("[Service Worker] fetching something ...", event)
+    event.respondWith(fetch(event.request))
+})
